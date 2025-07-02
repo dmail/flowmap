47e9b4b273474d6c91a7b7f04494d2fc23983c22
dmail <dmaillard06@gmail.com>
2025-07-02 17:02:40 +0200

    can change animations of flowmap layer

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,13 @@
 import { Deck } from "@deck.gl/core";
 import { getViewStateForLocations } from "@flowmap.gl/data";
 import { FlowmapLayer } from "@flowmap.gl/layers";
+import { effect, signal } from "@preact/signals";
 import { csv } from "d3-fetch";
 import { render } from "preact";
 
 const DATA_PATH = `https://gist.githubusercontent.com/ilyabo/68d3dba61d86164b940ffe60e9d36931/raw/a72938b5d51b6df9fa7bba9aa1fb7df00cd0f06a`;
+
+const animationEnabledSignal = signal(false);
 
 const init = async () => {
   const [locations, flows] = await Promise.all([
@@ -43,28 +46,54 @@ const init = async () => {
     layers: [],
   });
 
-  const flowmapLayer = new FlowmapLayer({
-    id: "my-flowmap-layer",
-    data: { locations, flows },
-    pickable: true,
-    getLocationId: (loc) => loc.id,
-    getLocationLat: (loc) => loc.lat,
-    getLocationLon: (loc) => loc.lon,
-    getFlowOriginId: (flow) => flow.origin,
-    getFlowDestId: (flow) => flow.dest,
-    getFlowMagnitude: (flow) => flow.count,
-    getLocationName: (loc) => loc.name,
+  const createFlowmapLayer = (animationEnabled = false) =>
+    new FlowmapLayer({
+      id: "my-flowmap-layer",
+      data: { locations, flows },
+      pickable: true,
+      animationEnabled,
+      getLocationId: (loc) => loc.id,
+      getLocationLat: (loc) => loc.lat,
+      getLocationLon: (loc) => loc.lon,
+      getFlowOriginId: (flow) => flow.origin,
+      getFlowDestId: (flow) => flow.dest,
+      getFlowMagnitude: (flow) => flow.count,
+      getLocationName: (loc) => loc.name,
+    });
+
+  // Initialize with the first layer
+  deck.setProps({
+    layers: [createFlowmapLayer(animationEnabledSignal.value)],
   });
 
-  deck.setProps({
-    layers: [flowmapLayer],
+  effect(() => {
+    const animationEnabled = animationEnabledSignal.value;
+    // Create a new layer instance with updated props
+    const updatedLayer = createFlowmapLayer(animationEnabled);
+    deck.setProps({
+      layers: [updatedLayer],
+    });
   });
 
   render(<UI />, document.querySelector("#ui"));
 };
 
 const UI = () => {
-  return "hello world";
+  // https://github.com/visgl/flowmap.gl/blob/main/examples/react-app/src/App.tsx
+  return (
+    <div style="background: white;">
+      <label>
+        Animations:
+        <input
+          type="checkbox"
+          onChange={(e) => {
+            animationEnabledSignal.value = e.target.checked;
+          }}
+          checked={animationEnabledSignal.value}
+        />
+      </label>
+    </div>
+  );
 };
 
 await init();
